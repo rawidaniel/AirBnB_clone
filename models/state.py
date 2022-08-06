201416517a2ec699cad654b850cbb56d7b8b6ccc
Rawi Daniel <rawidaniel96@gmail.com>
2022-08-06 12:48:22 -0700

    class that represent state

---
+++ models/state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Module state
+
+Contain class State
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class that represent State
+
+    Attributes
+    ----------
+    name : str
+        The name of the state
+    """
+    name = ""
